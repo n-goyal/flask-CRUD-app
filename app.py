7e5fc608c8fb54e4836a6a19270e721bae3a9619
Nitin Goyal <goyalnitin634@gmail.com>
2020-05-03 17:35:25 +0530

    added user input for todo app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ngoya:test123@localhost:5432/todoDB'
 db = SQLAlchemy(app)
 
+# Model
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
@@ -15,10 +16,22 @@ class Todo(db.Model):
 
 db.create_all()
 
+# controller - post request listener
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    # Instructions for model
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    # updates the view
+    return redirect(url_for('index'))
+
+# controller
 @app.route('/')
 def index():
+    # views: index.html
     return render_template('index.html', data=Todo.query.all())
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
