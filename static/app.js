f81210169a1cc9cd56f770c10691844e92d4d743
Nitin Goyal <goyalnitin634@gmail.com>
2020-05-16 12:36:07 +0530

    updated comments

--- static/app.js
+++ static/app.js
@@ -1,24 +1,31 @@
+// script for index.html controller calls
+
+// enables task completion
+// getting elements by Id defined in template
 const checkboxes = document.querySelectorAll('.check-completed');
+//iterating over to check which one is checked
 for (let i = 0; i<checkboxes.length; i++){
     const checkbox = checkboxes[i];
-    checkbox.onchange = function(e){
+    // if state changes POST request to controller
+    checkbox.onchange = function(e){    //e==event
         console.log('event', e);
         const newCompleted = e.target.checked;
         const todoId = e.target.dataset['id']
+        // update element state without redirect to home page
         fetch('/todos/' + todoId + '/set-completed', {
             method: 'POST',
-            body: JSON.stringify({
-                'completed': newCompleted
+            body: JSON.stringify({  // format: JSON
+                'completed': newCompleted           // updated status
             }),
             headers:{
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json'  
             }
         })
         .then(function(){
-            document.getElementById('error').className= 'hidden';
+            document.getElementById('error').className= 'hidden';   // if error: call hidden div 
         })
         .catch(function() {
-            document.getElementById('error').className = '';
+            document.getElementById('error').className = '';    // else nothing
         })
     }
 }
